ea295ca9587a31180e6035c4b1fd073c9c677b41
4LanEnterprises <4.lan.enterprises@gmail.com>
2024-06-29 03:11:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    const targetUrl = 'https://www.brainyquote.com/quote_of_the_day';
+    const proxyUrl = 'https://your-cors-proxy.herokuapp.com/proxy?url=';
+    const targetUrl = encodeURIComponent('https://www.brainyquote.com/quote_of_the_day');
 
     fetch(proxyUrl + targetUrl, {
         method: 'GET',
